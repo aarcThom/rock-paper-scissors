419d21f56cc535861f55903695e3326f2e17c260
aarcThom <thomas@aarc.io>
2022-10-18 02:19:35 -0700

    Started work on status bar

--- rps.js
+++ rps.js
@@ -1,69 +1,61 @@
+function getPlayerChoice() {
+    if (!playerChoice){
+            waitMsg(1000, itmChoiceMessage, 10);
+    commandLine.textContent = 'What will you choose? Rock, paper, or scissors?';
+    }
+}
+
+function waitMsg(delay, messageArr, repeats) {
+
+    let repeatArray = Array(repeats).fill(messageArr).flat();
+
+    for (let i = 0; i < repeatArray.length; i++) {
+        let addDelay = i * delay;
+        waitMsgs[i] = setTimeout(() => statusLine.textContent = `${repeatArray[i]}`, addDelay);
+    }
+}
+
 const gameChoices = ['rock', 'paper', 'scissors']
 
-function getCompChoice() {
-    let randInt = Math.round(Math.random() * 2);
-    return gameChoices[randInt];
-}
+//the game buttons
+const rockBut = document.querySelector('#rockBut');
+const paperBut = document.querySelector('#paperBut');
+const scissorsBut = document.querySelector('#scissorsBut');
 
-function getPlayerChoice() {
-    return prompt('Do you choose: rock, paper, or scissors?').toLowerCase();
-}
+//the command line and statusline
+const commandLine = document.querySelector('#commandLine');
+const statusLine = document.querySelector('#statusLine');
 
-function gameRound() {
-    let pChoice = getPlayerChoice();
-    let cChoice = getCompChoice();
-    
-    if (!gameChoices.includes(pChoice)) {
-        alert('You entered and incorrect value! Please re-enter your choice.');
-        return [0 ,0];
-    } 
+//the current player choice
+let playerChoice;
 
-    alert(`The robot chose ${cChoice}!`);
+//are we prompting for player input?
+let needInput = true;
 
-    if (pChoice === cChoice) {
-        alert(`You both chose ${pChoice}!`);
-        return [0, 0];
-    } else {
-        let match = pChoice+cChoice;
+//the bank of wait messages
+let waitMsgs = [];
+let itmChoiceMessage = ['|','/','--','\\']
 
-        switch (match) {
-            case 'rockscissors':
-                alert('Rock beats scissors! You rock!');
-                return [1, 0];
-            case 'scissorspaper':
-                alert('Scissors beats paper! You\'re a cut above the rest!');
-                return [1, 0];
-            case 'paperrock':
-                alert('Paper beats rock! You beat the robot to a pulp!');
-                return [1, 0];
-            case 'scissorsrock':
-                alert('Rock beats scissors! The robot rocked you!');
-                return [0, 1];
-            case 'paperscissors':
-                alert('Scissors beats paper! The robot cut you down to size!');
-                return [0, 1];
-            case 'rockpaper':
-                alert('Paper beats rock! The robot beat you to a pulp!');
-                return [0, 1];
-        }
+//the choice selection buttons
+const pSelectButtons = document.querySelectorAll('.selectBut');
+pSelectButtons.forEach((e) => {
+    e.addEventListener('click', () => {
+        if (needInput){
+            playerChoice = e.textContent;
+            for (message in waitMsgs){
+                clearTimeout(message);
+            };
+            waitMsgs = [];
+            needInput = false;
+            commandLine.textContent = `You chose ${playerChoice}!`;
+        }   
+    });
+});
+
+function mainLoop(){
+    if (needInput) {
+        getPlayerChoice();
     }
-
-}
-
-function mainLoop() {
-    let scoreCard = [0, 0];
-    while (!scoreCard.includes(5)){
-        let scoreUpdate = gameRound();
-        scoreCard = scoreCard.map((a, i) => a + scoreUpdate[i]);
-
-        if (scoreCard[0] === 5) {
-            alert(`You beat the robot ${scoreCard[0]} to ${scoreCard[1]}!`)
-        } else if (scoreCard[1] === 5) {
-            alert(`The robot beat you ${scoreCard[1]} to ${scoreCard[0]}!`)
-        } else {
-            alert(`The score is ${scoreCard[0]} to ${scoreCard[1]}`);
-        }  
-    }
-}
+};
 
 mainLoop();
